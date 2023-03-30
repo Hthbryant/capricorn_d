37e3003fdad6ad9f88a501a7282c5b231cc63f1d
huang <1635618490@qq.com>
2023-03-30 20:40:46 +0800

    Initial commit

---
+++ src/main/java/com/lingyi/capricorn_d/CapricornDApplication.java
@@ -0,0 +1,13 @@
+package com.lingyi.capricorn_d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapricornDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapricornDApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lingyi/capricorn_d/CapricornDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingyi.capricorn_d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapricornDApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
